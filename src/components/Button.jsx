3b17821bef87635dc4af29093b08a5e3429e22bf
Maverick Seneris <your_email@example.com>
2025-10-04 14:37:46 +0800

    feat: sticky add button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,7 +2,7 @@ export default function Button({ children, handler, position }) {
   return (
     <button
       onClick={handler}
-      className={`border-4 border-[#4F5858] rounded-2xl ${position}`}
+      className={`fixed border-4 border-[#4F5858] rounded-2xl ${position}`}
     >
       {children}
     </button>
